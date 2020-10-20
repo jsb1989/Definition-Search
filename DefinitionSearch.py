8cea80a5c0a0df4032274c74ccbb80f383fcf68f
Joshua Barnett <51182988+jsb1989@users.noreply.github.com>
2020-10-20 14:34:52 -0400

    Update DefinitionSearch.py
    Finished having the program provide a definition of a word per dictionary.com using Beautiful Soup, I also added error handling if the word is inputted wrong it will display a message saying so. Lastly I had it get the second definition to provide a more accurate result.

--- DefinitionSearch.py
+++ DefinitionSearch.py
@@ -1,12 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
 
 wikipage='https://en.wikipedia.org/wiki/'
 dictionary='https://www.dictionary.com/browse/'
-dictionaryend='?s=t'
 search=input("Enter what you would like to search:")
 
-URL1=wikipage+search
-URL2=dictionary+search+dictionaryend
+URL=dictionary+search
 
-page1=requests.get(URL1)
-page2=requests.get(URL2)
+page=requests.get(URL)
+dictionarysoup=BeautifulSoup(page.content,'html.parser')
+while True:
+	try:
+		definition=dictionarysoup.find('div',attrs={"value":"1"})
+		definition_text=definition.get_text()
+		print("Dictionary Definition: ")
+		print("- "+definition_text)
+		break
+	except AttributeError:
+		print("Not a valid word for the dictionary, please re-enter again correctly or try another word")
+		break
+
+while True:
+	try:
+		definition=dictionarysoup.find('div',attrs={"value":"2"})
+		definition_text=definition.get_text()
+		print("- "+definition_text)
+		break
+	except AttributeError:
+		break
